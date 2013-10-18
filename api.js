319cd538aa52d065887adbd75be34d26d57921df
Hassan Shirani <hshirani@localhost.localdomain>
2013-10-18 18:14:37 -0400

    - added outline of main api request function to api.js

--- api.js
+++ api.js
@@ -1,1 +1,12 @@
-function 
+// Function to make a request to the URI specified in parameter 1
+function apiRequest(uri, method, dataObj, successCallBack, failureCallback)
+{
+	responseObj = $.ajax(
+	{
+		type: method,
+		data: dataObj,
+		url: uri,
+	});
+
+	console.log(responseObj);
+}
