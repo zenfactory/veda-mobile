79c39dfba23a425b6b1638ad81182ab87ed14b2b
Hassan Benjamin Shirani <hshirani@zenfactory.org>
2013-11-06 05:33:19 +0000

    - almost done with quiz content building

--- api.js
+++ api.js
@@ -124,120 +124,15 @@ function buildQuizUri(contentArray)
 // Application Logic
 /////////////////////////////////
 
-getUris(restServices.uri);
-dbo(quizPaths);
+// Pull all the quiz URI's
+//getUris(restServices.uri);
 
-/*
-// Instantiate new data object
-var dataObj = new Object();                                                                                                                                                 
+// Instantiate quiz data object
+//var quizData = new Object();
 
-// Define uri to get sections from
-dataObj.uri = restServices.uri;
-
-// Define the request method (defaults to get)
-dataObj.method = "GET";
-
-// Make the request
-var response = apiRequest(restServices.uri, "GET");
-
-// Dump request to browser
-debugOut(response);
-
-// Instantiate quiz uri list container
-var quizUris = new Array();
-
-// Loop through the response's "children" object
-for (var x in response.children)
-{
-	// Dump debug to browser
-	debugOut(x);
-	debugOut(response.children[x]);
-
-	// Find the CHW Training data child
-	if (response.children[x].name == "CHW Training")
-	{
-		// Dump debug to browser
-		debugOut(response.children[x].path);
-
-		// Define the URI of the course we need data for 
-		var courseUri = response.children[x].path;
-
-		// Make request
-		var courseResponse = apiRequest(response.children[x].path, "GET")
-
-		// dump debug to browser
-		debugOut(courseResponse);
-
-		// Loop through course response children 
-		for (var x in courseResponse.children)
-		{
-			// Dump debug to browser
-			debugOut(courseResponse.children[x]);
-		
-			// Check that we are working with an active module
-			if (courseResponse.children[x].name == "CHW Training")
-			{
-				var chapterResponse = apiRequest(courseResponse.children[x].path, "GET");
-
-				// Dump debug to browser
-				debugOut(chapterResponse);
-
-				// Loop through subjects 
-				for (var x in chapterResponse.children)
-				{
-					// Dump debug to browser
-					debugOut(chapterResponse.children[x].path);
-					var chapterDataResponse = apiRequest(chapterResponse.children[x].path, "GET");
-
-					// Dump debug to browser
-					debugOut(chapterDataResponse);
-
-					// Loop through courses 
-					for (var x in chapterDataResponse.children)
-					{
-						// Dump debug to browser
-						debugOut(chapterDataResponse.children[x]);
-						var sectionResponse = apiRequest(chapterDataResponse.children[x].path, "GET");
-
-						// Dump debug to browser
-						debugOut(sectionResponse)
-
-						// Loop through lesson object
-						for (var x in sectionResponse.children)
-						{
-							var quizDataUriArray = sectionResponse.children[x].path.split('/');
-							var ndx = parseInt(quizDataUriArray.length, 10) - parseInt(2, 10);
-							quizDataUriArray[ndx] = "quiz";
-							quizDataUriArray.pop();
-
-							// Dump debug to browser
-							debugOut(quizDataUriArray);
-
-							// Instantiate quiz uri string
-							var quizUri = "/";
-
-
-							// Loop through the uri components and rebuild the uri
-							for (var x in quizDataUriArray)
-							{
-								if (x != 0)
-								{
-									quizUri += quizDataUriArray[x] + "/";
-								}
-
-								// Dump debug to browser
-								debugOut(quizUri);
-							
-							}
-							quizUris.push(quizUri);
-						}
-					}
-				}
-			}
-		}
-	}
-}
-
-// Dump debug to browser
-debugOut(quizUris);
-*/
+// Loop through the quiz uri's
+//for (var x in quizPaths)
+//{
+	//var sectionData = apiRequest(quizPaths[x], "GET");
+	//quizData[x] = sectionData;
+//}
